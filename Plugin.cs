d6b9c538fd868ee60adc5f543273ed422e85d937
Adam <a.clettborn@gmail.com>
2016-09-09 21:28:39 +0200

    Fixed what visual studio fucked up for me

--- Plugin.cs
+++ Plugin.cs
@@ -115,7 +115,7 @@ namespace PrivateMessage
             // Send the PM (The player who receives the PM)
             UnturnedChat.Say(toPlayer, string.Format(Translate("whisper_received"), fromPlayerName, message), UnturnedChat.GetColorFromName(Configuration.Instance.Color, Color.magenta));
             // Inform the sender that the message was sent (will look like a conversation, so you can scroll back to see what you actually wrote)
-            UnturnedChat.Say(fromPlayer, string.Format(Translate("whisper_sent"), toPlayer.DisplayName, message), Color.magenta);
+            UnturnedChat.Say(fromPlayer, string.Format(Translate("whisper_sent"), toPlayer.DisplayName, message), UnturnedChat.GetColorFromName(Configuration.Instance.Color, Color.magenta));
         }
     }
 }
